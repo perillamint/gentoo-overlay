b708697fe6ec82aa78e0ebbdfcac3d4ac789c15e
perillamint <perillamint@gmail.com>
2015-09-19 16:47:19 +0000

    firefox-nightly-bin

--- www-client/firefox-nightly-bin/files/all-gentoo.js
+++ www-client/firefox-nightly-bin/files/all-gentoo.js
@@ -1,6 +1,7 @@
-pref("app.update.enabled",                 false);
-pref("app.update.autoInstallEnabled",      false);
-pref("browser.display.use_system_colors",  true);
+lockPref("app.update.auto", false);
+lockPref("app.update.enabled", false);
+
+pref("browser.display.use_system_colors",  false);
 pref("browser.shell.checkDefaultBrowser",  false); 
 pref("browser.EULA.override",              true); 
 pref("intl.locale.matchOS",                true);
